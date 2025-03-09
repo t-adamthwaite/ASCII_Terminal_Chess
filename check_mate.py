bb10690c15b236bf3a4f9f5ed7743a4b48c28e9f
t-adamthwaite <200815567+t-adamthwaite@users.noreply.github.com>
2025-03-09 17:38:43 -0400

    add piece location search

--- check_mate.py
+++ check_mate.py
@@ -1,5 +1,19 @@
 import pieces as p
 
+#Initialize empty board
 board = p.Board()
-board.starting()
-print(board.state())
+
+#Populate board with pieces
+minus_pawns = []
+for i in range(0, 8):
+    pawn = p.MinusPawn()
+    minus_pawns.append(str(pawn))
+
+board.starting(minus_pawns)
+
+#Display current board
+#print(str(board))
+#print(str(board.rowA["A"][0]))
+
+#board.location("-P3")
+
--- pieces.py
+++ pieces.py
@@ -19,13 +19,42 @@ class Board:
 
         self.rowH = {"H": ['___', '___', '___', '___', '___', '___', '___', '___']}
 
-    def state(self):
+    def __str__(self):
         return str(self.row0) + '\n\n' + str(self.rowA) + '\n\n' + str(self.rowB) + '\n\n' + str(self.rowC) + '\n\n' + str(self.rowD) + '\n\n' + str(self.rowE) + '\n\n' + str(self.rowF) + '\n\n' + str(self.rowG) + '\n\n' + str(self.rowH)
     
-    def starting(self):
+    def starting(self, minus_pawns):
         self.rowA["A"] = ['-R-', '-N-', '-B-', '-Q-', '-K-', '-B-', '-N-', '-R-']
-        for i in range (0, 8):
-            self.rowB["B"][i] = '-P-'
+        self.rowB["B"] = minus_pawns
         for i in range (0, 8):
             self.rowG["G"][i] = '+P+'
-        self.rowH["H"] = ['+R+', '+N+', '+B+', '+Q+', '+K+', '+B+', '+N+', '+R+']
+        self.rowH["H"] = ['+R+', '+N+', '+B+', '+K+', '+Q+', '+B+', '+N+', '+R+']
+
+    def location(self, piece_id):
+        piece_id = piece_id
+        board_dict = self.rowA | self.rowB | self.rowC | self.rowD | self.rowE | self.rowF | self.rowG | self.rowH
+        #if piece_id in board_dict.values():
+        for key, list in board_dict.items():
+            for item in list:
+                if item == piece_id:
+                    print("Piece is located in Row {CHAR}, column {NUM}.".format(CHAR = key, NUM = list.index(item)+1))
+                    return True
+        #else:
+          #  print("Piece not on board.")
+
+
+class MinusPawn:
+    pawn_count = 0
+    text = '-P'
+    def __init__(self, first_move = False):
+        MinusPawn.pawn_count += 1
+        self.id = MinusPawn.text + str(MinusPawn.pawn_count)
+        self.first_move = first_move
+    
+    def __str__(self):
+        return self.id
+    
+    def move(self):
+        piece_id = input(["Piece id: "])
+        if Board.location(piece_id) == True:
+            print('hello')
+
